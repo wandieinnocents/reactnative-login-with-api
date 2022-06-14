e51472d0087336230eb41da0ba26ccd8785b61b3
wandieinnocents <wandieinnocent2@gmail.com>
2022-06-15 00:09:12 +0300

    Add handlers

--- screens/Register.js
+++ screens/Register.js
@@ -14,6 +14,19 @@ export default function Register({ navigation }) {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [isLoading, setIsLoading] = useState(false);
+
+  const onChangeNameHandler = (name) => {
+    setName(name);
+  };
+
+  const onChangeEmailHandler = (email) => {
+    setEmail(email);
+  };
+
+  const onChangePasswordHandler = (password) => {
+    setPassword(password);
+  };
+    
     
     return (
         <View style={styles.container}>
